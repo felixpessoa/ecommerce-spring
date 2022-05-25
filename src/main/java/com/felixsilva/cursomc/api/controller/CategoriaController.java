d9696ea059c27db9bed0fe66b698b3560f4a3cea
Gato Felix <Gato Felix@host.docker.internal>
2022-05-25 13:44:56 -0300

    Inserindo uma nova categoria com o post

--- src/main/java/com/felixsilva/cursomc/api/controller/CategoriaController.java
+++ src/main/java/com/felixsilva/cursomc/api/controller/CategoriaController.java
@@ -1,12 +1,17 @@
 package com.felixsilva.cursomc.api.controller;
 
+import java.net.URI;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felixsilva.cursomc.domain.model.Categoria;
 import com.felixsilva.cursomc.domain.service.CategoriaService;
@@ -28,6 +33,14 @@ public class CategoriaController {
 	public ResponseEntity<Categoria> findByIdCategoria(@PathVariable Integer categoriaId){
 		Categoria obj = categoriaService.findById(categoriaId);
 		return ResponseEntity.status(HttpStatus.OK).body(obj); 
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = categoriaService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getCategoriaId()).toUri(); 
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
