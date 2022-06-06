29a8768839bace5be7e7d290396d1028d2e6feac
Felix Pessoa Da Silva <felix.silva@interne.com.br>
2022-06-06 16:28:34 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/felixsilva/cursomc/api/controller/exception/FieldMessage.java
@@ -0,0 +1,29 @@
+package com.felixsilva.cursomc.api.controller.exception;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FieldMessage implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldName;
+	private String message;
+	public FieldMessage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public FieldMessage(String fieldName, String message) {
+		super();
+		this.fieldName = fieldName;
+		this.message = message;
+	}
+	
+	
+	
+	
+
+}
