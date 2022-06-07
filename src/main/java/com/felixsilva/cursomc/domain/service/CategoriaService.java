b14a7927551f9d78ad721c43d971338765e1ca42
Gato Felix <Gato Felix@host.docker.internal>
2022-06-07 15:04:35 -0300

    Paginação com parametros opcionais na requisiçao

--- src/main/java/com/felixsilva/cursomc/domain/service/CategoriaService.java
+++ src/main/java/com/felixsilva/cursomc/domain/service/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.felixsilva.cursomc.domain.model.Categoria;
@@ -48,6 +51,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 
 }
