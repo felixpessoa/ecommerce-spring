584fbfc964ceb5809f24033b2fd9a7edc8e241bd
Felix Pessoa Da Silva <felix.silva@interne.com.br>
2022-06-10 12:13:42 -0300

    Subtotal e total do pedido

--- src/main/java/com/felixsilva/cursomc/domain/model/Pedido.java
+++ src/main/java/com/felixsilva/cursomc/domain/model/Pedido.java
@@ -2,8 +2,8 @@ package com.felixsilva.cursomc.domain.model;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -19,8 +19,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
-@Entity
 @Data
+@Entity
 public class Pedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -42,22 +42,35 @@ public class Pedido implements Serializable{
 	
 	@ManyToOne
 	@JoinColumn(name = "endereco_de_entrega_id")
-	private Endereco enderecoDeEntre;
+	private Endereco enderecoDeEntrega;
 	
-	@OneToMany(mappedBy = "id.pedido")
-	private Set<ItemPedido> itens = new HashSet<>();
+	@OneToMany(mappedBy="id.pedido")
+	private List<ItemPedido> itens = new ArrayList<>();
 	
 	public Pedido() {
 		super();
 	}
 
-	public Pedido(Integer pedidoId, LocalDateTime instante, Cliente cliente, Endereco enderecoDeEntre) {
+	public Pedido(Integer pedidoId, LocalDateTime instante, Cliente cliente, Endereco enderecoDeEntrega) {
 		super();
 		this.pedidoId = pedidoId;
 		this.instante = instante;
 		this.cliente = cliente;
-		this.enderecoDeEntre = enderecoDeEntre;
+		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+	
+	
+	
+	
 	
 	
 
